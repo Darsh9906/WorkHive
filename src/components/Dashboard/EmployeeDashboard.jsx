421c1fd6246c4361a1e20f3e309b26e5d090e095
Darsh <darshbhatt089@gmail.com>
2026-06-17 23:49:32 +0530

    Added tasklist in the dashboard UI

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -8,8 +8,8 @@ const EmployeeDashboard = ({data}) => {
   return (
     <div className='p-10 bg-[#1c1c1c] h-screen'>
         <Header data={data}/>
-        <TaskListNumber />
-        <TaskList />
+        <TaskListNumber data={data} />
+        <TaskList data={data}/>
     </div>
   )
 }
--- src/components/EmployeeDashboard/TaskListNumber.jsx
+++ src/components/EmployeeDashboard/TaskListNumber.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
-const TaskListNumber = () => {
+const TaskListNumber = ({data}) => {
   return (
     <div className='flex justify-between mt-10 gap-5 '>
         <div className='bg-red-400 flex-1 rounded-xl px-8 py-5 hover:scale-105'>
-            <h2 className='font-bold text-2xl'>0</h2>
+            <h2 className='font-bold text-2xl'>{data.taskNumber.newTask}</h2>
             <h3  className='font-medium text-2xl'>New Task</h3>
         </div>
 
         <div className='bg-blue-400 flex-1 rounded-xl px-8 py-5 hover:scale-105'>
-            <h2 className='font-bold text-2xl'>0</h2>
-            <h3  className='font-medium text-2xl'>New Task</h3>
+            <h2 className='font-bold text-2xl'>{data.taskNumber.completed}</h2>
+            <h3  className='font-medium text-2xl'>Completed Task</h3>
         </div> 
 
-        <div className='bg-yellow-400 flex-1 rounded-xl px-8 py-5 hover:scale-105'>
-            <h2 className='font-bold text-2xl'>0</h2>
-            <h3  className='font-medium text-2xl'>New Task</h3>
+        <div className='bg-yellow-400 flex-1 text-black rounded-xl px-8 py-5 hover:scale-105'>
+            <h2 className='font-bold text-2xl'>{data.taskNumber.active}</h2>
+            <h3  className='font-medium text-2xl'>Accepted Task</h3>
         </div>
 
         <div className='bg-green-400 flex-1 rounded-xl px-8 py-5 hover:scale-105'>
-            <h2 className='font-bold text-2xl'>0</h2>
-            <h3  className='font-medium text-2xl'>New Task</h3>
+            <h2 className='font-bold text-2xl'>{data.taskNumber.failed}</h2>
+            <h3  className='font-medium text-2xl'>Failed Task</h3>
         </div>
     
     </div>
